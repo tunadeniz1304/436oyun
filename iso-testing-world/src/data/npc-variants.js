4702bafa3aab1ac9a7996c6503fc882d5ef81044
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2026-05-10 13:39:09 +0300

    .

--- iso-testing-world/src/data/npc-variants.js
+++
@@ -1,8 +0,0 @@
-export const NPC_VARIANTS = {
-  'suit-m':   { gender: 'm', outfit: 'suit',   hair: '#1e293b', skin: '#fcd34d' },
-  'suit-f':   { gender: 'f', outfit: 'suit',   hair: '#3a2418', skin: '#f6c8a8' },
-  'casual-m': { gender: 'm', outfit: 'casual', hair: '#5a3a20', skin: '#e3a87c' },
-  'casual-f': { gender: 'f', outfit: 'casual', hair: '#1a1a1a', skin: '#d4a37a' },
-  'lab-m':    { gender: 'm', outfit: 'lab',    hair: '#2a1a10', skin: '#fcd34d' },
-  'lab-f':    { gender: 'f', outfit: 'lab',    hair: '#6b4423', skin: '#f0c896' },
-};
